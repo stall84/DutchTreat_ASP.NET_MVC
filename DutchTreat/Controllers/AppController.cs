f1df0cc53a4c2d06a57ba23b6011744c1c6f7673
Michael Stallings <stall84@gmail.com>
2020-09-19 12:16:01 -0400

    Initial commit, much of the building is already completed. Starting to add Bootstrap styling

---
+++ DutchTreat/Controllers/AppController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DutchTreat.Services;
+using DutchTreat.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace DutchTreat.Controllers
+{
+    public class AppController : Controller
+    {
+        private readonly IMailService _mailService;
+
+        public AppController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            ViewBag.Title = "Contact Us";
+
+            return View();
+        }
+
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            ViewBag.Title = "Contact Us";
+
+            if (ModelState.IsValid)
+            {   // setting up a mock-email relay to trigger when form passes validation. 
+                // 'templating' in the fields from our form-model
+                _mailService.SendMessage("stall84@gmail.com", model.Subject, $"From: {model.Name} - {model.Email}, Message: {model.Message}");
+                ViewBag.EmailMessage = $"Email successfully sent! - From: {model.Name} -- Subject:  {model.Subject} -- Message:  {model.Message} .";
+                ModelState.Clear();
+            }
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "About Us";
+
+            return View();
+        }
+    }
+}
---
+++ DutchTreat/ViewModels/ContactViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DutchTreat.ViewModels
+{
+    public class ContactViewModel
+    {
+        [Required]
+        [MinLength(5, ErrorMessage = "Name has to be at least 5 characters")]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        [MaxLength(100, ErrorMessage = "100 characters or Less")]
+        public string Message { get; set; }
+    }
+}
